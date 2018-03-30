ad42c69b7b6b7c92ea4c1c12b6e166a041eeef0d
RaymondChen <raymondchen@RaymondChen-MacBook-Pro.local>
2018-03-31 04:45:14 +0800

    init

---
+++ Assets/Scripts/stone.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class stone : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnBecameInvisible()  
+	{  
+		if(this.transform.position.z>=0)Destroy(this.gameObject);  
+	}  
+}
